7f18d4915b9c2f4e16da92c52251ea1ade872aa7
Emerson Jordan <emerz210@gmail.com>
2024-01-28 14:39:02 -0800

    add splat sound perfect

---
+++ Assets/FlyMusic.cs
@@ -0,0 +1,236 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FlyMusic : MonoBehaviour
+
+
+{
+
+        [SerializeField] private EventReference flyMusic;
+
+        [SerializeField] private EventReference fallSound;
+        [SerializeField] private EventReference splatSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        StartCoroutine(FlyBeginning());
+         StartCoroutine(SplatSound());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SplatSound()
+    {
+        yield return new WaitForSecondsRealtime(6f);
+        AudioManager.instance.PlayOneShot(splatSound, this.transform.position);
+        yield return new WaitForSecondsRealtime(3.1f);
+        AudioManager.instance.PlayOneShot(fallSound, this.transform.position);
+
+    }
+
+    private IEnumerator FlyBeginning()
+    {
+        int counter = 0;
+        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        
+        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.04f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.2f);
+                        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        
+        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.04f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        
+        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.04f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        
+        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.04f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        
+        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.04f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        
+        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.04f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+               yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+               yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+               yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+               yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+               yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+               yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+               yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+               yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+               yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+               yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+               yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        
+        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.04f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        
+        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.04f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        
+        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.04f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        
+        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.04f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        
+        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.04f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        
+        yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.04f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.1f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+
+                yield return new WaitForSecondsRealtime(.2f);
+        AudioManager.instance.PlayOneShot(flyMusic, this.transform.position);
+        
+    }
+}
+
+
