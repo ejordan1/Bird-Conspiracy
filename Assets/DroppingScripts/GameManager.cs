3d865d757981f621472aabf044fba0943fba2183
Emerson Jordan <emerz210@gmail.com>
2024-01-28 08:26:06 -0800

    Refactor game scripts
    refactor game scripts, take out previous manual code

--- Assets/DroppingScripts/GameManager.cs
+++ Assets/DroppingScripts/GameManager.cs
@@ -8,24 +8,13 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
 
-    //public List<GameObject> targetObjects;
-    private List<PoopTarget> poopTargets;
 
     public string nextSceneString;
 
-    public Camera camera;
-    // Start is called before the first frame update
+
     void Start()
     {
 
-        camera = Camera.main;
-        poopTargets = new List<PoopTarget>();
-        GameObject[] targetObjects = GameObject.FindGameObjectsWithTag("Target");
-        foreach (GameObject g in targetObjects)
-        {
-            poopTargets.Add(g.GetComponent<PoopTarget>());
-
-        }
     }
 
     public void clickedOnBackground()
@@ -36,53 +25,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    //     if (Input.GetMouseButtonDown(0))
-    //     {
 
-    //        Vector3 mousePosition = Input.mousePosition;
-    //        Ray ray = camera.ScreenPointToRay(mousePosition);
-    //        if (Physics.Raycast(ray, out RaycastHit hit))
-    //        {
-    //         Debug.Log(hit.)
-    //            // Use the hit variable to determine what was clicked on.
-    //        }
-    //    }
-            // Vector3 mousePos = Input.mousePosition;
-            // bool targetHit = false;
-
-            // foreach (PoopTarget poopTarget in poopTargets)
-            // {
-            //     if ((Mathf.Abs(mousePos.x - poopTarget.gameObject.transform.position.x) < poopTarget.xSize) && Math.Abs(mousePos.y - poopTarget.gameObject.transform.position.y) < poopTarget.ySize)
-            //     {
-            //         poopTarget.GotPooped();
-            //         targetHit = true;
-            //     }
-            // }
-            // if (!targetHit)
-            // {
-            //     // game over
-            // }
-
-        //}
-
-        // if (targetsAllHit())
-        // {
-        //     StartCoroutine(MoveCoroutine());
-        //     //start ending sequence
-        //     // Debug.Log("All targets hit");
-        // }
-    }
-
-    private bool targetsAllHit()
-    {
-        foreach (PoopTarget p in poopTargets)
-        {
-            if (!p.hasBeenHit)
-            {
-                return false;
-            }
-        }
-        return true;
     }
 
     private IEnumerator MoveCoroutine()
--- Assets/DroppingScripts/PoopTarget.cs
+++ Assets/DroppingScripts/PoopTarget.cs
@@ -11,10 +11,7 @@ public class PoopTarget : MonoBehaviour
 
     public Image poop;
     public Image check;
-
-    public float ySize = 40;
-    public float xSize = 6;
-
+    
     void Start()
     {
         poop.GetComponent<Image>().color = new Color32(255,255,225,0);
@@ -23,16 +20,6 @@ public class PoopTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetMouseButtonDown(0))
-        // {
-        //     Vector3 mousePos = Input.mousePosition;
-            
-        //     if ((Mathf.Abs(mousePos.x - this.transform.position.x) < 6) && Math.Abs(mousePos.y - this.transform.position.y) < 40)
-        //     {
-                
-        //     }
-            
-        // }
     }
 
     public void GotPooped()
